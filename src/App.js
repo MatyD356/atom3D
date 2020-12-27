a875d31735acee6e7f132ffbe9b5f83431f2642e
Mateusz <matyd356@gmail.com>
2020-12-27 12:35:58 +0100

    add electrons

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const createSphere = (r = 1, color = 0x61DAFB) => {
   const sphereMat = new THREE.MeshPhysicalMaterial({
     color,
   })
-  const sphereGeo = new THREE.SphereGeometry(r, 20, 20)
+  const sphereGeo = new THREE.SphereGeometry(r, 10, 10)
   return new THREE.Mesh(sphereGeo, sphereMat)
 }
 
@@ -35,10 +35,10 @@ const nucleus = createSphere(3)
 const l1 = createPointLight(.8)
 const l2 = createPointLight(.4)
 l1.position.set(60, 20, 60)
+
 l2.position.set(-30, 0, 20)
 
-scene.add(nucleus, l2)
-nucleus.add(l1)
+scene.add(nucleus, l1, l2)
 
 const createElectron = (r = .4, color = 0x61DAFB) => {
   const sphere = createSphere(r, color)
@@ -56,31 +56,38 @@ const e3 = createElectron(.4)
 const e4 = createElectron(.4)
 
 e1.sphere.position.set(10, 0, 0)
-e2.sphere.position.set(5, 0, 0)
-e3.sphere.position.set(-5, 0, 0)
-e4.sphere.position.set(-10, 0, 0)
+e2.sphere.position.set(10, 0, 0)
+e3.sphere.position.set(10, 0, 0)
+e4.sphere.position.set(10, 0, 0)
 
-/* const geometry = new THREE.TorusGeometry(10, .1, 4, 100);
+const geometry = new THREE.TorusGeometry(10, .1, 4, 100);
 const material = new THREE.MeshPhysicalMaterial({ color: 0x61DAFB });
-const torus = new THREE.Mesh(geometry, material); */
+const torus1 = new THREE.Mesh(geometry, material);
+const torus2 = new THREE.Mesh(geometry, material);
+const torus3 = new THREE.Mesh(geometry, material);
 
-nucleus.add(e1.pivot, e2.pivot, e3.pivot, e4.pivot)
 
-e1.pivot.rotation.y = 120
-e2.pivot.rotation.y = 60
-e3.pivot.rotation.y = -60
-e4.pivot.rotation.y = -90
+nucleus.add(torus1, torus2, torus3, e1.pivot, e2.pivot, e3.pivot)
+
 
 
 const loop = () => {
-  e1.pivot.rotation.z += 0.04
-  e2.pivot.rotation.z += 0.05
-  e3.pivot.rotation.z += 0.03
-  e4.pivot.rotation.z += 0.06
+  torus1.rotation.x += 0.01
+  torus1.rotation.y += 0.01
+  torus2.rotation.x += 0.01
 
   nucleus.rotation.z += 0.001
   nucleus.rotation.x += 0.002
   nucleus.rotation.y += 0.003
+
+  e1.pivot.rotation.z += 0.1
+
+  e2.pivot.rotation.x += 0.01
+  e2.pivot.rotation.y += 0.01
+  e2.pivot.rotation.z += 0.1
+
+  e3.pivot.rotation.x += 0.01
+  e3.pivot.rotation.z += 0.09
 
   renderer.render(scene, camera)
   requestAnimationFrame(loop)
